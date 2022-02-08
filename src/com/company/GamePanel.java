ac3f59e71acab0ee56d5819b5c8c3f6bf7deedad
MB211187 <maxburtonphillips@outlook.com>
2022-02-08 16:10:36 +0000

    Initial commit

---
+++ src/com/company/GamePanel.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; //16*16
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    Thread gameThread;
+
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.yellow);
+        this.setDoubleBuffered(true);
+    }
+    @Override
+    public void run(){
+        
+    }
+
+
+}
